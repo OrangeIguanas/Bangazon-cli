f81be76d7247e08896ecefdfcf11b60796f57860
Abby Moore <abbyleighanne@gmail.com>
2017-02-08 15:51:10 -0600

    create and test Order

---
+++ Methods/CreateOrder.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class Order():
+    """
+    Purpose: Allow a Customer to create an order
+    Author: Abby
+    Methods: get_customer, get_order_complete, set_order_to_completed
+    """
+
+    def __init__(self, customer, order_complete):
+        self.__customer = customer
+        self.__order_complete = False
+
+
+
+    def get_customer(self):
+        return self.__customer.get_full_name()
+
+    def get_order_complete(self):
+        return self.__order_complete
+
+    def set_order_to_completed(self):
+    	self.__order_complete = True
+        return self.__order_complete
+
