4c2d9c96426ef7564fd1c860856c2016089fe745
Zachary Cline <zcline08@gmail.com>
2017-02-08 15:56:07 -0600

    fixed my tests to ACTUALLY work

--- tests/testPaymentMethod.py
+++ tests/testPaymentMethod.py
@@ -11,16 +11,21 @@ class TestPaymentMethod(unittest.TestCase):
 	def test_can_create_payment_method(self):
 		
 		
-		visa_card = PaymentMethod(card_number="4267520023493346", card_type="Visa", exp_date="07/20", cvv="333", name_on_card="Bob Bobbins", client="name")
+		self.visa_card = PaymentMethod(card_number="4267520023493346", card_type="Visa", exp_date="07/20", cvv="333", name_on_card="Bob Bobbins", client="name")
 		
-		self.assertIsInstance(visa_card, PaymentMethod)
+		self.assertIsInstance(self.visa_card, PaymentMethod)
 
 
 	def test_payment_method_required_properties(self):
 
 		self.bob = Customer(first_name = "Bob", last_name="Bobbins", address = "111 Front Street", city = "Smyrna" , state = "Tennessee" , postalZip = 37167, phone_number="615-999-1111", email="d@d.com")
-		visa_card = PaymentMethod(card_number="4267520023493346", card_type="Visa", exp_date="07/20", cvv="333", name_on_card="Bob Bobbins", client="name")
-		self.assertIsNotNone(visa_card)
+		self.visa_card = PaymentMethod(card_number="4267520023493346", card_type="Visa", exp_date="07/20", cvv="333", name_on_card="Bob Bobbins", client="name")
+		self.assertIsNotNone(self.visa_card.get_card_number())
+		self.assertIsNotNone(self.visa_card.get_name_on_card())
+		self.assertIsNotNone(self.visa_card.get_card_type())
+		self.assertIsNotNone(self.visa_card.get_exp_date())
+		self.assertIsNotNone(self.visa_card.get_cvv())
+		self.assertIsNotNone(self.visa_card.get_client())
 		
 if __name__ == "__main__":
 	unittest.main()
