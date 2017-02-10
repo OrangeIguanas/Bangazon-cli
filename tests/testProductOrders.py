3d45600d1aafc862d579474dc1c9b199c47371fb
Shawn M <forgeforza@outlook.com>
2017-02-10 13:35:46 -0600

    updated documentation on tests

--- tests/testProductOrders.py
+++ tests/testProductOrders.py
@@ -9,20 +9,20 @@ from Methods.CreateProductOrders import ProductOrders
 class TestProductOrders(unittest.TestCase):
 
 	"""
-	Test ProductOrders for access to product or order ids. 
+	Test ProductOrders for access to product or order ids which are FOREIGN KEYS .  
 	"""
 
 	def test_productorders_has_product(self):
 
 		wonkavator = ProductOrders(product = '3', order = '5')
-
+		#check if the product foreign key is an instance of ProductOrders table.
 		self.assertIsInstance(wonkavator, ProductOrders)
 		print(wonkavator.get_product())  # Returns as product
 
 	def test_productorders_has_order(self):
 
 		wonkavator = ProductOrders( product = '3', order = '5')
-
+		#check if the product foreign key is an instance of ProductOrders table
 		self.assertIsInstance(wonkavator, ProductOrders)
 		print(wonkavator.get_order())
 
