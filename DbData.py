1b3e22c4b4c4b4e07748dec0676783517de83ee9
Paul Martin <Dmart331@gmail.com>
2017-02-13 10:51:37 -0600

    Added data to customers

--- DbData.py
+++ DbData.py
@@ -7,12 +7,16 @@ c = conn.cursor()
 
 
 
-customers = [(3, 'Drew', 'Martin' , 'Drew@drew.com', '615-481-0464' , 'Nashville', 'Tennessee', 37122, '1111 Back' , 'False') , (4, 'Zach' , 'Cline', 'Zach@z.com', '615-111-2222' , 'Nashville', 'Tennessee' , 37167, '2222 Block', 'False'), (5, 'Pete', 'Staggs' , 'Pete@pete.pete' , '617-222-3332' , 'Mt. Juliet' , 'Tennessee' , 37111, '444 Hole Road', 'False')]
+customers = [(1, 'Bob', 'Bobbins', 'd@d.com', '615-999-1111', 'Smyrna', 'Tennessee', 37167, '111 Front Street', 'False'), (2, 'Joe', 'Mammy', 'joe@mammy.com', '625-444-4444', 'Smyrna', 'Tennessee', 38933, '222 Back', 'False'), (3, 'Drew', 'Martin', 'Drew@drew.com', '615-481-0464', 'Nashville', 'Tennessee', 37122, '1111 Back', 'False'), (4, 'Zach', 'Cline', 'Zach@z.com', '615-111-2222', 'Nashville', 'Tennessee', 37167, '2222 Block', 'False'), (5, 'Pete', 'Staggs', 'Pete@pete.pete', '617-222-3332', 'Mt. Juliet', 'Tennessee', 37111, '444 Hole Road', 'False'), (6, 'Ben', 'Marks', 'Ben@ben.ben', '617-432-2344', 'Nashville', 'Tennessee', 37131, '321 countdown road', 'False'), (7, 'Loki', 'Destroyer', 'BringHell@space.com', '123-456-7890', 'Asgard', 'Valhalla', 0, 'Good Luck', 'False')]
 
 c.executemany('INSERT INTO Customers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', customers)
 
 c.execute('SELECT * FROM Customers')
 print(c.fetchall())
+
+conn.commit()
+
+conn.close()
 
 # for row in c.execute('SELECT * FROM Customers ORDER BY customer_id'):
 # 	print row
