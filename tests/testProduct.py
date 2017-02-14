521a199fb39951c50086b8beb3c71d4236b09ade
Zachary Cline <zcline08@gmail.com>
2017-02-14 14:02:09 -0600

    docs for product tests

--- tests/testProduct.py
+++ tests/testProduct.py
@@ -7,7 +7,15 @@ from Methods.CreateCategory import Category
 
 
 class ProductTest(unittest.TestCase):
-
+	"""
+	Purpose: Test Products
+	Author: Peter, Zach
+	Tests: 
+		test_can_register_product_to_database
+		test_can_create_product
+		test_can_update_product_qty_when_a_user_adds_to_their_cart
+		test_product_has_properties
+	"""
 	@classmethod
 	def setUpClass(self):
 
@@ -22,22 +30,26 @@ class ProductTest(unittest.TestCase):
 			qty = 5,
 			category_id = self.weapons)
 
+	def test_can_create_product(self):
+		#Testing if the bazooka object is a Product
+		self.assertIsInstance(self.Bazooka, Product)
+		print(self.Bazooka.get_description())
+		print(self.Bazooka.get_category_id())
 
 	def test_can_register_product_to_database(self):
+		#Testing if the Bazooka object contains the foreign key for the weapons category
+		# Bazooka(Product) is reliant upon weapons(Category) so it must be created to the database first
 		self.weapons.save_category(self.weapons)
 		self.Bazooka.register_product(self.Bazooka)
 
 	
 	def test_can_update_product_qty_when_a_user_adds_to_their_cart(self):
+		# Test if Bazooka quantity decreases by one each time it is added to a customer cart.
 		self.Bazooka.update_product(self.Bazooka, 1)
 		
-	def test_can_create_product(self):
-		self.assertIsInstance(self.Bazooka, Product)
-		print(self.Bazooka.get_description())
-		print(self.Bazooka.get_category_id())
 
 	def test_product_has_properties(self):
-
+		# Testing if Bazooka Has All Required Field Properties
 		self.assertIsNotNone(self.Bazooka.get_name())
 		self.assertIsNotNone(self.Bazooka.get_price())
 		self.assertIsNotNone(self.Bazooka.get_qty())
