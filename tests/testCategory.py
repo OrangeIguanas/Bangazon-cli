11db514e5a9a560406d13d1d825815177069aa04
Zachary Cline <zcline08@gmail.com>
2017-02-14 14:19:42 -0600

    added docs to category tests

--- tests/testCategory.py
+++ tests/testCategory.py
@@ -7,21 +7,34 @@ from Methods.CreateCategory import Category
 
 class TestCategory(unittest.TestCase): 
 
+	"""
+	Purpose: Test Category
+	Author: Zach
+	Tests: 
+		test_can_create_a_category
+		test_can_register_category_to_database
+
+	"""
+
 	@classmethod
 	def setUpClass(self):
 		self.food = Category(
 			category_name = "Food")
 
 
-	def test_can_create_a_category(self): 
+	def test_can_create_a_category(self):
+		# Test is food a type of Category
 		self.assertIsInstance(self.food, Category)
 
 	def test_can_register_category_to_database(self):
+		# Test if Food can be added to the database
+		# Test if Food is in the Database
 		self.food.save_category(self.food)
 		self.assertTrue(Category.category_is_registered(self.food))
 
 
-	def test_category_has_product(self):
+	
+
 		
 
 
